418d4a5476d640a7869fab24adf7bcf4b3722124
Drakkar <blacktor666@gmail.com>
2020-08-31 22:19:31 +0300

    create new project

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let money;
+
+let income;
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period = 'Your smile the best';
+
+alert(period)
+
+console.log('Учимся до потери пулься');
